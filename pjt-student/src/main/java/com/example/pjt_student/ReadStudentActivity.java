c64646429d3515ab50c76d3aadf27a12d2c8a5f2
Jackie <crazyshet@hotmail.com>
2016-10-18 16:27:36 +0900

    tab 추가

--- pjt-student/src/main/java/com/example/pjt_student/ReadStudentActivity.java
+++ pjt-student/src/main/java/com/example/pjt_student/ReadStudentActivity.java
@@ -1,13 +1,62 @@
 package com.example.pjt_student;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 public class ReadStudentActivity extends AppCompatActivity {
+
+    ImageView studentImage;
+    TextView nameView;
+    TextView phoneView;
+    TextView emailView;
+
+    TabHost host;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_student_activity);
+
+        initData();
+        initTab();
+    }
+
+    private void initData() {
+        studentImage = (ImageView)findViewById(R.id.read_student_image);
+        nameView = (TextView)findViewById(R.id.read_name);
+        phoneView = (TextView)findViewById(R.id.read_phone);
+        emailView = (TextView)findViewById(R.id.read_email);
+    }
+
+    private void initTab() {
+        host = (TabHost)findViewById(R.id.host);
+        host.setup(); // 너네 하위에 TabWidget과 FrameLayout 선언되어 있을 거다
+                      // 선언되어 있는데로 ui 준비하라
+        // tab 하나가 TabSpec으로 표현되고 Tab Spec에 button과 button을 눌렀을 때 나오는 view가 연결되어 표현
+        // 그 버튼 누르면 무조건 그 view가 출력
+        // 매게변수는 개발자가 주는 tab 식별자 문자열
+        // 필요없을 수도 있고 코드에서 유저가 어느 tab 화면을 보고 있는건지 판단할 때 이용
+        TabHost.TabSpec tabSpec =host.newTabSpec("tab1");
+        tabSpec.setIndicator("SCORE");//button
+        tabSpec.setContent(R.id.read_score_list);
+        host.addTab(tabSpec);
+
+        tabSpec =host.newTabSpec("tab2");
+        tabSpec.setIndicator("CHART");//button
+        tabSpec.setContent(R.id.read_score_chart);
+        host.addTab(tabSpec);
+
+        tabSpec =host.newTabSpec("tab3");
+        tabSpec.setIndicator("ADD");//button
+        tabSpec.setContent(R.id.read_score_add);
+        host.addTab(tabSpec);
+
+        tabSpec =host.newTabSpec("tab4");
+        tabSpec.setIndicator("MEMO");//button
+        tabSpec.setContent(R.id.read_memo);
+        host.addTab(tabSpec);
     }
 }
